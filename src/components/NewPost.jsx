7fe55683a61497e60358f4b15a80fb7f23a606fc
Kelly Kingston <okellykings220@gmail.com>
2024-03-28 02:57:16 +0100

    Update and fetch data from node js Db backend

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,42 @@
-import style from './NewPost.module.css';
-function NewPost(props) {
-    
+import style from "./NewPost.module.css";
+import { useState } from "react";
+function NewPost({ onCancel, onAddPost }) {
+  const [currentValue, SetNewValue] = useState("I Love React");
+  const [currentName, SetNewName] = useState("Kelly Kingston");
+
+  function changeBodyHandler(event) {
+    SetNewValue(event.target.value);
+  }
+
+  function changeNameHandler(event) {
+    SetNewName(event.target.value);
+  }
+
+  function formSubmit(event) {
+    event.preventDefault();
+    const PostData = {
+      body: currentValue,
+      author: currentName,
+    };
+    onAddPost(PostData);
+    onCancel();
+  }
+
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={formSubmit}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.bodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={changeNameHandler} />
+      </p>
+      <p className={style.actions}>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,28 +1,51 @@
 import style from "./postLists.module.css";
 import Post from "./post";
 import NewPost from "./NewPost";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "./Modal";
 
 function PostList({ isOpen, isClose }) {
-  const [currentValue, SetNewValue] = useState("I Love React");
+  const [posts, setPosts] = useState([]);
 
-  function changeBodyHandler(event) {
-    SetNewValue(event.target.value);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+  function AddPost(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {isOpen && (
         <Modal onClose={isClose}>
-          <NewPost bodyChange={changeBodyHandler} />
+          <NewPost onCancel={isClose} onAddPost={AddPost} />
         </Modal>
       )}
-      <ul className={style.posts}>
-        <Post author="Kelly Kingston" body={currentValue} />
-        <Post author="Egbe Kliuvert" body="React is Awesome" />
-        <Post author="Diamond wilson" body="React is Awefull" />
-      </ul>
+      {(posts.length > 0 && (
+        <ul className={style.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )) || (
+        <div style={{ textAlign: "center" }}>
+          <h2> No Posts Found</h2>
+          <p>Add New Posts</p>
+        </div>
+      )}
     </>
   );
 }
